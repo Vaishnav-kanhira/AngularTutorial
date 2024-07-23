f4941b099e2bf51543817604a8c8739843468c42
Vaishnav-kanhira <vaishnav@mobcup.net>
2024-07-23 09:59:58 +0530

    switched to modules

--- T02/ClassTypes.ts
+++ T02/ClassTypes.ts
@@ -1,36 +1,4 @@
-class MathFunctions {
-    // valueOne: number
-    // valueTwo: number
-
-    // constructor(valueOne: number, valueTwo: number) {
-    //     this.valueOne = valueOne
-    //     this.valueTwo = valueTwo
-    // }
-
-    constructor(private _valueOne: number, private _valueTwo: number) { }
-
-    set valueOne(value: number) {
-        if (value > 5) {
-            this._valueOne = 5
-        } else {
-            this._valueOne = value
-        }
-    }
-
-    get valueOne(): number {
-        return this._valueOne
-    }
-
-    private add: () => number = () => (this._valueOne + this._valueTwo)
-    private substract: () => number = () => (this._valueOne - this._valueTwo)
-    private multiply: () => number = () => (this._valueOne * this._valueTwo)
-    private divide: () => number = () => (this._valueOne / this._valueTwo)
-    private modulus: () => number = () => (this._valueOne % this._valueTwo)
-    executeAll: () => string = () => {
-        return `valueOne = ${this.valueOne} | _valueTwo = ${this._valueTwo}\n` +
-            `add = ${this.add()} | substract = ${this.substract()} | multiply = ${this.multiply()} | divide = ${this.divide()} | modulus = ${this.modulus()}`
-    }
-}
+import { MathFunctions } from "./MathFunctions"
 
 let mathFunctions = new MathFunctions(23, 89)
 mathFunctions.valueOne = 20
