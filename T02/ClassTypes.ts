c48ceeb2f6e41f313c40e1deecb84c308f9ca40f
Vaishnav-kanhira <vaishnav@mobcup.net>
2024-07-23 09:54:32 +0530

    setter and getter added

--- T02/ClassTypes.ts
+++ T02/ClassTypes.ts
@@ -7,18 +7,31 @@ class MathFunctions {
     //     this.valueTwo = valueTwo
     // }
 
-    constructor(private valueOne: number, private valueTwo: number) { }
+    constructor(private _valueOne: number, private _valueTwo: number) { }
 
-    private add: () => number = () => (this.valueOne + this.valueTwo)
-    private substract: () => number = () => (this.valueOne - this.valueTwo)
-    private multiply: () => number = () => (this.valueOne * this.valueTwo)
-    private divide: () => number = () => (this.valueOne / this.valueTwo)
-    private modulus: () => number = () => (this.valueOne % this.valueTwo)
+    set valueOne(value: number) {
+        if (value > 5) {
+            this._valueOne = 5
+        } else {
+            this._valueOne = value
+        }
+    }
+
+    get valueOne(): number {
+        return this._valueOne
+    }
+
+    private add: () => number = () => (this._valueOne + this._valueTwo)
+    private substract: () => number = () => (this._valueOne - this._valueTwo)
+    private multiply: () => number = () => (this._valueOne * this._valueTwo)
+    private divide: () => number = () => (this._valueOne / this._valueTwo)
+    private modulus: () => number = () => (this._valueOne % this._valueTwo)
     executeAll: () => string = () => {
-        return `add = ${this.add()} | substract = ${this.substract()} | multiply = ${this.multiply()} | divide = ${this.divide()} | modulus = ${this.modulus()}`
+        return `valueOne = ${this.valueOne} | _valueTwo = ${this._valueTwo}\n` +
+            `add = ${this.add()} | substract = ${this.substract()} | multiply = ${this.multiply()} | divide = ${this.divide()} | modulus = ${this.modulus()}`
     }
 }
 
 let mathFunctions = new MathFunctions(23, 89)
-
+mathFunctions.valueOne = 20
 console.log(mathFunctions.executeAll())
