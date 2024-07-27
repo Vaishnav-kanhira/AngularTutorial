02c5e14b776b88843e922a2d24ec11db600c26d4
Vaishnav-kanhira <vaishnav@mobcup.net>
2024-07-27 21:29:34 +0530

    todos listing is being displayed correctly

--- T01/Hello-World/src/app/todos/todo_listing/todo-listing/todo-listing.component.ts
+++ T01/Hello-World/src/app/todos/todo_listing/todo-listing/todo-listing.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../../../data/Todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-listing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-listing.component.html',
   styleUrl: './todo-listing.component.css'
 })
 export class TodoListingComponent {
+  todos: Todo[]
 
+  constructor() {
+    this.todos = [
+      new Todo(0, "title 1", "description 1", false),
+      new Todo(0, "title 2", "description 2", false),
+      new Todo(0, "title 3", "description 3", false),
+      new Todo(0, "title 4", "description 4", false),
+    ]
+  }
 }
