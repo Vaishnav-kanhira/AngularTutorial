c7e3304d29d58d4a1ac476144a8a51ad066b7f1e
Vaishnav-kanhira <vaishnav@mobcup.net>
2024-07-27 21:24:06 +0530

    2 new components added

---
+++ T01/Hello-World/src/app/todos/todo_listing/todo-listing/todo-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-listing.component.html',
+  styleUrl: './todo-listing.component.css'
+})
+export class TodoListingComponent {
+
+}
